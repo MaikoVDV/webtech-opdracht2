37987b23dc7d51f8d59822b48419e2acc8430f22
Maiko van der Veen <maikovdveen@gmail.com>
2025-03-10 23:58:53 +0100

    JSON files being read and converted to JS objects. Began basic class structuring.

--- scripts/file-upload.js
+++ scripts/file-upload.js
@@ -1,7 +1,35 @@
 window.addEventListener("load", () => {
-  console.log("haaai xx");
   var fileUpload = document.getElementById("card__file-input");
-  fileUpload.addEventListener("change", () => {
-    console.log("changed input");
-  });
+
+  fileUpload.addEventListener("change", handleFileUpload);
 });
+
+function handleFileUpload(event) {
+  const file = event.target.files[0];
+
+  if (!file) {
+    // No file was entered.
+    console.warn("Changed file input but did not select any file.");
+    return;
+  }
+  if (file.type != "application/json") {
+    console.warn(`Selected a file with the wrong extension: ${file.type}`);
+  }
+
+  const reader = new FileReader();
+  reader.addEventListener("error", () => {
+    console.error(`An unknown error occurred while reading ${file.name}`);
+  });
+  reader.addEventListener("load", () => {
+    readUploadedData(reader.result);
+  });
+
+  reader.readAsText(file);
+
+  console.log(`Selected ${file.name}`);
+}
+
+function readUploadedData(jsonString) {
+  var uploadedObject = JSON.parse(jsonString);
+  console.log(uploadedObject);
+}
---
+++ scripts/person.js
@@ -0,0 +1,13 @@
+class Person {
+  #firstName;
+  #lastName;
+
+  get firstName() { return this.#firstName; }
+  get lastName() { return this.#lastName; }
+}
+class Student extends Person {
+
+}
+class Course {
+    
+}

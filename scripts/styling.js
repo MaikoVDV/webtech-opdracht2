b221d67bbbb9d581b2517a158ce770830e6a3773
Maiko van der Veen <maikovdveen@gmail.com>
2025-03-14 23:04:57 +0100

    Add comments and remove unused code.

--- scripts/styling.js
+++ scripts/styling.js
@@ -3,6 +3,7 @@ let styleDropdownList;
 let elDropdownHeader;
 let selectedEl;
 
+// Links CSS selectors to the properties that can be assigned to those elements.
 const selectorMap = [
   { selector: "body", properties: ["color", "background", "font-size"] },
   { selector: "article", properties: ["color", "background", "font-size"] },
@@ -10,6 +11,7 @@ const selectorMap = [
   { selector: "header", properties: ["color", "background", "font-size"] },
   { selector: "footer", properties: ["color", "background", "font-size"] },
 ];
+// Links CSS properties to the types of inputs needed to address them in the popup.
 const styleMap = [
   { property: "color", inputs: ["color"] },
   { property: "background", inputs: ["color"] },
@@ -17,15 +19,16 @@ const styleMap = [
 ]
 
 window.addEventListener("load", () => {
+  // Accessing by id instead of class (possibly violating BEM?) for better maintainability and readability.
   elDropdownList = document.getElementById("element-selector-list");
   styleDropdownList = document.getElementById("style-selector-list");
   elDropdownHeader = document.getElementById("element-selector-header");
-  console.log(elDropdownHeader);
 
   updateStylableElementList();
   updateStylablePropertiesList();
 });
 
+// Fills Element dropdown with list of elements available for styling.
 function updateStylableElementList() {
   if (!elDropdownList) return; // Prevents error due to script loading delays
   elDropdownList.replaceChildren();
@@ -39,24 +42,36 @@ function updateStylableElementList() {
     });
   });
 }
+
+// Fills style dropdown with list of available style properties for the element selected with the Element dropdown.
 function updateStylablePropertiesList() {
   styleDropdownList.replaceChildren();
-  if (!selectedEl) return;
+  if (!selectedEl) {
+    styleDropdownList.previousElementSibling.classList.add("nav-dropdown__button--empty");
+    return;
+  }
 
   let properties = getStyleableProperties(selectedEl);
+  // Rounds nav-dropdown__button if the dropdown is empty.
+  if (properties.length == 0) {
+    styleDropdownList.previousElementSibling.classList.add("nav-dropdown__button--empty");
+  } else {
+    styleDropdownList.previousElementSibling.classList.remove("nav-dropdown__button--empty");
+  }
+
   properties.forEach(property => {
     addItemToDropdown(styleDropdownList, property, (clickedProp) => {
       let inputs = styleMap.find(mapping => {
         return mapping.property == clickedProp;
       }).inputs;
-      let index = 0;
+      let index = 0; // Ensures unique name for inputs.
       inputs = inputs.map(input => {
         index++;
-        return { type: input, name: `${input}${index}`}
-      })
+        return { type: input, name: `${input}${index}` };
+      });
+
       createPopup(`Select ${property} for ${selectedEl.tagName.toLowerCase()}`, inputs,
         (output) => {
-          console.log(output[inputs[0]]);
           selectedEl.style[property] = output[inputs[0].name];
         });
     });
@@ -76,8 +91,6 @@ function getStyleableElements() {
 }
 
 function getStyleableProperties(element) {
-  // console.log(selectorMap[{ selector: element.tagName.toLowerCase() }]);
-  // console.log(element.tagName.toLowerCase());
   return selectorMap.find(mapping => {
     return mapping.selector == element.tagName.toLowerCase(); 
   }).properties;
